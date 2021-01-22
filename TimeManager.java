2e128b37dfb0096f3bbd0c0ad7a6cb99a404c182
Spencer Gable-Cook <sgablec@umich.edu>
2021-01-22 17:40:47 -0500

    Implemented logic

--- TimeManager.java
+++ TimeManager.java
@@ -27,8 +27,36 @@ class TimeManager {
 
     // Convert the hours and minutes into the total number of minutes after midnight.
     // This way, we can easily add numberOfMinutes to this value to get the new time
-    int totalMinutes = hour * 60 + minutes;
+    int totalMinutes = (hour % 12) * 60 + minutes;
+    // If the time is PM, then add the number of minutes at noon
+    if (timeOfDay.equals("PM")) {
+      totalMinutes += NUMBER_MINUTES_NOON;
+    }
+    // Add 'numberOfMinutes' to this total
+    totalMinutes += numberOfMinutes;
+    // Check if 'totalMinutes' overflows the total minutes in a day.
+    // If this is the case, then it must wrap around to the next day.
+    if (totalMinutes >= NUMBER_MINUTES_DAY) {
+      totalMinutes = totalMinutes % NUMBER_MINUTES_DAY;
+    }
+    System.out.println(totalMinutes);
 
-    return "Hello";
+    // Now, convert the raw minutes back into a time format
+    String newTimeOfDay = "AM";
+    // First, get if the new time is AM or PM
+    if (totalMinutes >= NUMBER_MINUTES_NOON) {
+      totalMinutes -= NUMBER_MINUTES_NOON;
+      newTimeOfDay = "PM";
+    }
+    // Convert totalMinutes back to hour and minute format
+    int newHour = totalMinutes / 60;
+    if (newHour == 0) {
+      newHour = 12;
+    }
+    int newMinutes = totalMinutes % 60;
+
+    // Finally, create string that will contain the new time
+    String newTime = Integer.toString(newHour) + ":" + Integer.toString(newMinutes) + " " + newTimeOfDay;
+    return newTime;
   }
 }
